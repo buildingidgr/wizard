79de4e9332b40a97472812dc8a0c0202aed49595
buildingidgr <78495205+buildingidgr@users.noreply.github.com>
2024-11-27 10:29:15 +0200

    df

--- next.config.js
+++ next.config.js
@@ -3,7 +3,16 @@ const nextConfig = {
   output: 'standalone',
   experimental: {
     serverActions: true
-  }
+  },
+  // Ensure that the root path is served
+  async rewrites() {
+    return [
+      {
+        source: '/',
+        destination: '/index',
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
