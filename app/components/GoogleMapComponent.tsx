1b33b4eb39afa1892c5688f54287a70b7b08b6b1
buildingidgr <78495205+buildingidgr@users.noreply.github.com>
2024-11-27 16:19:09 +0200

    sdf

--- app/components/GoogleMapComponent.tsx
+++ app/components/GoogleMapComponent.tsx
@@ -15,7 +15,7 @@ const GoogleMapComponent: React.FC<GoogleMapComponentProps> = ({ onAddressSelect
   })
 
   const [center, setCenter] = React.useState({ lat: 0, lng: 0 })
-  const [zoom, setZoom] = React.useState(2) // Start with a zoomed-out view
+  const [zoom, setZoom] = React.useState(2)
   const [address, setAddress] = React.useState('')
   const autocompleteRef = React.useRef<google.maps.places.Autocomplete | null>(null)
   const mapRef = React.useRef<google.maps.Map | null>(null)
@@ -24,6 +24,177 @@ const GoogleMapComponent: React.FC<GoogleMapComponentProps> = ({ onAddressSelect
     width: '100%',
     height: '100%'
   }
+
+  const mapStyles = [
+    {
+        "featureType": "all",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "weight": "2.00"
+            }
+        ]
+    },
+    {
+        "featureType": "all",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#9c9c9c"
+            }
+        ]
+    },
+    {
+        "featureType": "all",
+        "elementType": "labels.text",
+        "stylers": [
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "all",
+        "stylers": [
+            {
+                "color": "#f2f2f2"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape.man_made",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "all",
+        "stylers": [
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": 45
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#eeeeee"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#7b7b7b"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "labels.text.stroke",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "all",
+        "stylers": [
+            {
+                "color": "#46bcec"
+            },
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#c8d7d4"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#070707"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "labels.text.stroke",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            }
+        ]
+    }
+  ]
 
   const handlePlaceSelect = () => {
     const place = autocompleteRef.current?.getPlace()
@@ -34,8 +205,7 @@ const GoogleMapComponent: React.FC<GoogleMapComponentProps> = ({ onAddressSelect
       setAddress(place.formatted_address || '')
       onAddressSelect(place.formatted_address || '', lat, lng)
 
-      // Zoom in and center the map
-      setZoom(17) // Set a closer zoom level
+      setZoom(17)
       if (mapRef.current) {
         mapRef.current.panTo({ lat, lng })
       }
@@ -84,6 +254,11 @@ const GoogleMapComponent: React.FC<GoogleMapComponentProps> = ({ onAddressSelect
           center={center}
           zoom={zoom}
           onLoad={onMapLoad}
+          options={{
+            styles: mapStyles,
+            disableDefaultUI: true,
+            zoomControl: true,
+          }}
         >
           {center.lat !== 0 && center.lng !== 0 && <Marker position={center} />}
         </GoogleMap>
