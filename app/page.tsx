7912c1b09cf512d57886f4e6fa5d50a4579fb4c6
buildingidgr <78495205+buildingidgr@users.noreply.github.com>
2024-11-27 12:47:09 +0200

    dfg

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@ import ProjectTypeSelector from './components/ProjectTypeSelector'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <div className="bg-blue-500 text-white p-4 mb-4 rounded">
+        If you can see this blue box with white text, Tailwind CSS is working!
+      </div>
       <ProjectTypeSelector />
     </main>
   )
