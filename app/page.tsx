97127da3299532c39992374c0418e2a398729275
buildingidgr <78495205+buildingidgr@users.noreply.github.com>
2024-11-27 19:05:33 +0200

    sdf

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import React from 'react'
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card"
 import ProjectTypeSelector from './components/ProjectTypeSelector'
 import ProjectAddressForm from './components/ProjectAddressForm'
 import ProjectDetailsForm from './components/ProjectDetailsForm'
